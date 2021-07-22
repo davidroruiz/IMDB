094ae490ae37a66b4e10afe70a296fec960695fb
AmandaMToro <amandamorillo17@gmail.com>
2021-07-22 15:39:57 +0200

    fix(Professional): Añado getters and setters

--- professional.ts
+++ professional.ts
@@ -12,6 +12,103 @@ class Professional
     public nationality: string;
     public oscarNumbers: number;
     public profession: string;
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public getAge(): number {
+        return this.age;
+    }
+
+    public setAge(age: number): void {
+        this.age = age;
+    }
+
+    public getGenre(): string {
+        return this.genre;
+    }
+
+    public setGenre(genre: string): void {
+        this.genre = genre;
+    }
+
+    public getWeight(): number {
+        return this.weight;
+    }
+
+    public setWeight(weight: number): void {
+        this.weight = weight;
+    }
+
+    public getHeight(): number {
+        return this.height;
+    }
+
+    public setHeight(height: number): void {
+        this.height = height;
+    }
+
+    public getHairColor(): string {
+        return this.hairColor;
+    }
+
+    public setHairColor(hairColor: string): void {
+        this.hairColor = hairColor;
+    }
+
+    public getEyeColor(): string {
+        return this.eyeColor;
+    }
+
+    public setEyeColor(eyeColor: string): void {
+        this.eyeColor = eyeColor;
+    }
+
+    public getRace(): string {
+        return this.race;
+    }
+
+    public setRace(race: string): void {
+        this.race = race;
+    }
+
+    public isIsRetired(): boolean {
+        return this.isRetired;
+    }
+
+    public setIsRetired(isRetired: boolean): void {
+        this.isRetired = isRetired;
+    }
+
+    public getNationality(): string {
+        return this.nationality;
+    }
+
+    public setNationality(nationality: string): void {
+        this.nationality = nationality;
+    }
+
+    public getOscarNumbers(): number {
+        return this.oscarNumbers;
+    }
+
+    public setOscarNumbers(oscarNumbers: number): void {
+        this.oscarNumbers = oscarNumbers;
+    }
+
+    public getProfession(): string {
+        return this.profession;
+    }
+
+    public setProfession(profession: string): void {
+        this.profession = profession;
+    }
+
     
     constructor(name:string, age:number, genre:string, weigth:number,
                 height: number, hairColor: string, eyeColor: string, 
@@ -32,4 +129,5 @@ class Professional
             this.profession=profession;
         }
 
+        
 }
